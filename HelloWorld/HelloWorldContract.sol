3497a57a6f43e0d26a9376654d1bad30985a1c02
Kayode Ezike <kayodeezike@dhcp-18-111-24-106.dyn.mit.edu>
2017-08-23 18:10:43 -0400

    Added constructor for and permission-granting modifier for setter

--- HelloWorld/HelloWorldContract.sol
+++ HelloWorld/HelloWorldContract.sol
@@ -1,13 +1,26 @@
 pragma solidity ^0.4.0;
 
 contract HelloWorldContract {
+
     string greeting = "Hello World";
-  
+    address public issuer;
+
+    function HelloWorldContract() {
+        issuer = msg.sender;
+    }
+
+    modifier ifIssuer() {
+        if (issuer != msg.sender) {
+            throw;
+        }
+        _;
+    }
+
     function getGreeting() constant returns(string) {
         return greeting;
     }
 
-    function setGreeting(string newGreeting) {
+    function setGreeting(string newGreeting) ifIssuer {
         greeting = newGreeting;
     }
 }
